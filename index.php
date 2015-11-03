0e14e61b544700e59ba7e4aa5c7efc1df7fadf39
DennisSenac66 <dennistadm34@gmail.com>
2015-11-03 16:43:43 -0200

    modificado o index.php

--- index.php
+++ index.php
@@ -3,8 +3,23 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
 
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, " . $name;
+// Hello World
+$app->get('/hello/:name', function ($name) { 
+    $inteiro1 = 42; // tipo Integer
+    $inteiro2 = 79; // tipo Integer
+    $texto = "Ola, "; // tipo String
+    $fracao = 2.3; // tipo Float
+    
+    $soma = $inteiro1 + $inteiro2;
+    $divisao = $inteiro1 / $fracao;
+    $multi = $inteiro2 * $fracao;
+    
+    $olaMundo = $texto . $name;
+    
+    echo "<p>Soma: " . $soma . "</p>";
+    echo "<p>Divisao: " . $divisao . "</p>";
+    echo "<p>Multiplica: " . $multi . "</p>";
+    echo "<p>" . $olaMundo . "</p>";
 });
 
 $app->run();
